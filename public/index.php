f5b6da3ddd220335e1361931f3c68cd4e06bb4b1
Konstantin <levachin@luckandroll.com>
2019-12-15 19:12:23 +0300

    change response sender

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-use Framework\Http\ResponseSender;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 
@@ -19,5 +19,5 @@ $response = (new HtmlResponse('Hello, ' . $name . '!'))
 
 ### Sending
 
-$emitter = new ResponseSender();
-$emitter->send($response);
+$emitter = new SapiEmitter();
+$emitter->emit($response);
