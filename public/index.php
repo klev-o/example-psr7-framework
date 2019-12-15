dd9481864e1ea8c0386c5031708c091a3a0cb55b
Konstantin <levachin@luckandroll.com>
2019-12-15 19:08:25 +0300

    improved response sending

--- public/index.php
+++ public/index.php
@@ -17,8 +17,19 @@ $response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-MyHeader', 'Hello World');
 
 ### Sending
-header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+//header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+header(sprintf(
+    'HTTP/%s %d %s',
+    $response->getProtocolVersion(),
+    $response->getStatusCode(),
+    $response->getReasonPhrase()
+));
+
+
 foreach ($response->getHeaders() as $name => $values) {
-    header($name . ':' . implode(', ', $values));
+    //header($name . ':' . implode(', ', $values));
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
 }
 echo $response->getBody();
