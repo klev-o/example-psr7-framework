f3d3268a22e633a3739c49b5a46bc9431121fe75
Konstantin <levachin@luckandroll.com>
2019-12-20 23:27:27 +0300

    added default handler

--- public/index.php
+++ public/index.php
@@ -31,9 +31,7 @@ $routes->get('cabinet', '/cabinet', function (ServerRequestInterface $request) u
     $pipeline->pipe(new Middleware\ProfilerMiddleware());
     $pipeline->pipe(new Middleware\BasicAuthMiddleware($config['users']));
     $pipeline->pipe(new Action\CabinetAction());
-    return $pipeline($request, function () {
-        return new HtmlResponse('Undefined page', 404);
-    });
+    return $pipeline($request, new Middleware\NotFoundHandler());
 });
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog.show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
@@ -54,7 +52,8 @@ try {
     $action = $resolver->resolve($result->getHandler());
     $response = $action($request);
 } catch (RequestNotMatchedException $e){
-    $response = new HtmlResponse('Undefined page', 404);
+    $handler = new Middleware\NotFoundHandler();
+    $response = $handler($request);
 }
 
 ### Postprocessing
