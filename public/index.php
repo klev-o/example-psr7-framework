adae83024ef7a96d1d4f6b26d4bfcc4464007854
Konstantin <levachin@luckandroll.com>
2019-12-20 23:13:35 +0300

    added profiler middleware

--- public/index.php
+++ public/index.php
@@ -26,10 +26,13 @@ $routes = $aura->getMap();
 $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('cat', '/cat', Action\CatAction::class);
 $routes->get('cabinet', '/cabinet', function (ServerRequestInterface $request) use ($config) {
+    $profiler = new Middleware\ProfilerMiddleware();
     $auth = new Middleware\BasicAuthMiddleware($config['users']);
     $cabinet = new Action\CabinetAction();
-    return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
-        return $cabinet($request);
+    return $profiler($request, function (ServerRequestInterface $request) use ($auth, $cabinet) {
+        return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
+            return $cabinet($request);
+        });
     });
 });
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
