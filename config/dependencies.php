9fe0c346b7fe5f799eeac8b3ea94951e7ff2a7ec
Konstantin <levachin@luckandroll.com>
2020-01-19 15:23:56 +0300

    Added container constructor definitions

--- config/dependencies.php
+++ config/dependencies.php
@@ -8,23 +8,24 @@ use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Zend\Diactoros\Response;
 
-/** @var Container $container */
-$container->set(Application::class, function (Container $container) {
-    return new Application(
-        $container->get(MiddlewareResolver::class),
-        $container->get(Router::class),
-        new Middleware\NotFoundHandler()
-    );
-});
-$container->set(Router::class, function () {
-    return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-});
-$container->set(MiddlewareResolver::class, function (Container $container) {
-    return new MiddlewareResolver(new Response(), $container);
-});
-$container->set(Middleware\BasicAuthMiddleware::class, function (Container $container) {
-    return new Middleware\BasicAuthMiddleware($container->get('config')['users'], new Response());
-});
-$container->set(Middleware\ErrorHandlerMiddleware::class, function (Container $container) {
-    return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-});
+return [
+    Application::class => function (Container $container) {
+        return new Application(
+            $container->get(MiddlewareResolver::class),
+            $container->get(Framework\Http\Router\Router::class),
+            new Middleware\NotFoundHandler()
+        );
+    },
+    Router::class => function () {
+        return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+    },
+    MiddlewareResolver::class => function (Container $container) {
+        return new MiddlewareResolver(new Response(), $container);
+    },
+    Middleware\BasicAuthMiddleware::class => function (Container $container) {
+        return new Middleware\BasicAuthMiddleware($container->get('config')['users'], new Response());
+    },
+    Middleware\ErrorHandlerMiddleware::class => function (Container $container) {
+        return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
+    },
+];
