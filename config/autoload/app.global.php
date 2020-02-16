be9e63d3bb373bd42e7710617b534c357fdd4545
Konstantin <levachin@luckandroll.com>
2020-02-16 14:12:00 +0300

    Extracted templates config

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -6,9 +6,6 @@ use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Framework\Template\TemplateRenderer;
-use Framework\Template\Php\PhpRenderer;
-use Framework\Template\Twig\Extension\RouteExtension;
-use Framework\Template\Twig\TwigRenderer;
 use Psr\Container\ContainerInterface;
 use Zend\Diactoros\Response;
 
@@ -36,38 +33,7 @@ return [
                     $container->get('config')['debug'],
                     $container->get(TemplateRenderer::class)
                 );
-            },
-            TemplateRenderer::class => function (ContainerInterface $container) {
-                //return new PhpRenderer('views', $container->get(Router::class));
-//                $renderer = new PhpRenderer('views');
-//                $renderer->addExtension($container->get(RouteExtension::class));
-//                return $renderer;
-                return new TwigRenderer($container->get(Twig\Environment::class), '.html.twig');
-            },
-            Twig\Environment::class => function(ContainerInterface $container)
-            {
-                $templateDir = 'views';
-                $cacheDir = 'var/cache/twig';
-                $debug = $container->get('config')['debug'];
-
-                $loader = new Twig\Loader\FilesystemLoader();
-                $loader->addPath($templateDir);
-
-                $environment = new Twig\Environment($loader, [
-                    'cache' => $debug ? false : $cacheDir,
-                    'debug' => $debug,
-                    'strict_variables' => $debug,
-                    'auto_reload' => $debug,
-                ]);
-
-                if ($debug) {
-                    $environment->addExtension(new Twig\Extension\DebugExtension());
-                }
-
-                $environment->addExtension($container->get(RouteExtension::class));
-
-                return $environment;
-            },
+            }
         ],
     ],
     'debug' => true,
