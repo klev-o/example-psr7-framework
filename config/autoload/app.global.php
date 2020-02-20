6e8cb62ede2ca90cb40201b71893650bec0df4d3
Konstantin <levachin@luckandroll.com>
2020-02-20 19:42:29 +0300

    Extracted error response generator

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Middleware;
+use App\Http\Middleware\ErrorHandler\PrettyErrorResponseGenerator;
+use App\Http\Middleware\ErrorHandler\ErrorHandlerMiddleware;
+use App\Http\Middleware\ErrorHandler\ErrorResponseGenerator;
 use Framework\Http\Application;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
@@ -29,8 +32,13 @@ return [
             MiddlewareResolver::class => function (ContainerInterface $container) {
                 return new MiddlewareResolver(new Response(), $container);
             },
-            Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
-                return new Middleware\ErrorHandlerMiddleware(
+            ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
+                return new ErrorHandlerMiddleware(
+                    $container->get(ErrorResponseGenerator::class)
+                );
+            },
+            ErrorResponseGenerator::class => function (ContainerInterface $container) {
+                return new PrettyErrorResponseGenerator(
                     $container->get('config')['debug'],
                     $container->get(TemplateRenderer::class)
                 );
--- config/pipeline.php
+++ config/pipeline.php
@@ -7,7 +7,8 @@ use Framework\Http\Application;
 /** @var Application $app */
 
 //$app->pipe($container->get(Middleware\ErrorHandlerMiddleware::class));
-$app->pipe(Middleware\ErrorHandlerMiddleware::class);
+//$app->pipe(Middleware\ErrorHandlerMiddleware::class);
+$app->pipe(Middleware\ErrorHandler\ErrorHandlerMiddleware::class);
 $app->pipe(Middleware\CredentialsMiddleware::class);
 $app->pipe(Middleware\ProfilerMiddleware::class);
 //$app->pipe($container->get(Framework\Http\Middleware\RouteMiddleware::class));
