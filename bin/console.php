d861448315f3a1b82762ffd8b3ceabf38265c009
Konstantin <levachin@luckandroll.com>
2020-02-28 23:10:31 +0300

    Changed cache clear command

--- bin/console.php
+++ bin/console.php
@@ -11,5 +11,9 @@ require 'vendor/autoload.php';
  */
 $container = require 'config/container.php';
 
-$cli = new Application('Application console');
+$cli = new Application('Console App');
+$commands = $container->get('config')['console']['commands'];
+foreach ($commands as $command) {
+    $cli->add($container->get($command));
+}
 $cli->run();
