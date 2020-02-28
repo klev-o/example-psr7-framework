84f22ac38ae01f2fcaa02edac78d4db90312c077
Konstantin <levachin@luckandroll.com>
2020-02-28 18:24:54 +0300

    Added console commands list

--- bin/console.php
+++ bin/console.php
@@ -15,5 +15,10 @@ require 'vendor/autoload.php';
 $container = require 'config/container.php';
 
 $cli = new Application();
-$cli->add($container->get(CacheClearCommand::class));
+
+$commands = $container->get('config')['console']['commands'];
+foreach ($commands as $command) {
+    $cli->add($container->get($command));
+}
+
 $cli->run(new Input($argv), new Output());
